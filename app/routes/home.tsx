971a2996d42bc80ccfc7a1e5bc5577657195ef23
CamiloMaria <camilocraft08@gmail.com>
2025-09-18 14:42:28 -0400

    Add resume display functionality to Home component; render ResumeCard for each resume and handle empty state with a message.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -19,6 +21,21 @@ export default function Home() {
           <h2>Review your submissions and get feedback on your resume and cover letter.</h2>
         </div>
       </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+
+      {resumes.length === 0 && (
+        <div className="resumes-section">
+          <h1>No resumes found</h1>
+        </div>
+      )}
+
     </main>
   )
 }
