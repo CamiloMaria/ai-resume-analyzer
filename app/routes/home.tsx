01ad6a608c976a7568840723ae9d9cd853d42b74
CamiloMaria <camilocraft08@gmail.com>
2025-09-18 20:17:39 -0400

    Enhance ResumeCard and Home components to fetch and display resumes dynamically. Implement loading state in Home for better user experience, and update ResumeCard to handle resume image fetching and display fallback for missing data.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -11,6 +12,24 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { kv } = usePuterStore()
+  const [resumes, setResumes] = useState<Resume[]>([])
+  const [loadingResumes, setLoadingResumes] = useState(false)
+
+  useEffect(() => {
+    const fetchResumes = async () => {
+      setLoadingResumes(true)
+
+      const resumes = await kv.list('resume_*', true) as KVItem[]
+
+      const parsedResumes = resumes.map((resume) => JSON.parse(resume.value as string) as Resume)
+
+      setResumes(parsedResumes || [])
+      setLoadingResumes(false)
+    }
+    fetchResumes()
+  }, [])
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
@@ -18,10 +37,19 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Analysis</h1>
-          <h2>Review your submissions and get feedback on your resume and cover letter.</h2>
+
+          {!loadingResumes && resumes.length > 0 && (
+            <h2>Review your submissions and get feedback on your resume and cover letter.</h2>
+          )}
+
+          {loadingResumes && (
+            <div className="flex flex-col items-center justify-center">
+              <img src="/images/resume-scan-2.gif" alt="Loading resumes" className="w-[200px]" />
+            </div>
+          )}
         </div>
 
-        {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
@@ -29,7 +57,7 @@ export default function Home() {
           </div>
         )}
 
-        {resumes.length === 0 && (
+        {!loadingResumes && resumes.length === 0 && (
           <div className="resumes-section">
             <h1>No resumes found</h1>
           </div>
